733c6a126cf0f05c30fe342481acb1dd5d9778c8
Pedro Gabriel Tozzi <pedrogbtozzi@gmail.com>
2020-09-04 16:04:33 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ads20/lifecycle/ActivityDialog.java
@@ -0,0 +1,23 @@
+package br.usjt.ads20.lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class ActivityDialog extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_dialog);
+    }
+
+    public void finishDialog(View view) {
+        ActivityDialog.this.finish();
+    }
+}
